48d0370e9a213781ff8d2f5b7c37fb5b2f4484d2
AnnTumanina20_IT_17 <98600734+AnnTumanina@users.noreply.github.com>
2023-04-08 13:02:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laba_9_W
+{
+    public class Array<T>
+    {
+        List<T> list = new List<T> { };
+
+        public Array()
+        {
+        }
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+        public void Delete(T item)
+        {
+            list.Remove(item);
+        }
+        public T GetElem(int ind)
+        {
+            return list[ind];
+        }
+        public int GetLength()
+        {
+            return list.Count;
+        }
+    }
+
+    public class Login : Array<string>
+    {
+
+    }
+
+    public class Password<T> : Array<T>
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Login login = new Login();
+            login.Add("Meloni");
+
+            Password<string> password = new Password<string>();
+            password.Add("Ashley");
+        }
+    }
+}
